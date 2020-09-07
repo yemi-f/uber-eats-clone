ce695ad0363cdeeb1e3654bd029766b304044c21
Yemi Fakorede <yemifakorede@gmail.com>
2020-09-07 16:13:28 -0400

    added rating to restaurant cards

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Col, Card } from "react-bootstrap";
+import { Row, Col, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { getMenuUrl, getPriceRange, getHeartIcon } from "../utils/HelperFunctions";
 
@@ -18,6 +18,21 @@ const RestaurantCard = ({ restaurant }) => {
         )
     }
 
+    const getRatingSpan = (rating) => {
+        if (rating > 4.5) {
+            return (
+                <span className="rounded-circle text-center float-right" style={{ fontSize: "14px", backgroundColor: "#E6F2ED", color: "#05A357", width: "40px", height: "40px", padding: "8px" }}>
+                    {restaurant.rating.toFixed(1)}
+                </span>
+            )
+        }
+        return (
+            <span className="rounded-circle text-center float-right" style={{ fontSize: "14px", backgroundColor: "#EEEEEE", color: "#000000", width: "40px", height: "40px", padding: "8px" }}>
+                {restaurant.rating.toFixed(1)}
+            </span>
+        )
+    }
+
     return (
         <Col className="mt-4">
             <Link to={`/${getMenuUrl(restaurant.restaurantName)}`} style={{ textDecoration: "none", color: "inherit" }}>
@@ -26,12 +41,19 @@ const RestaurantCard = ({ restaurant }) => {
                     <Card.ImgOverlay className="d-flex justify-content-end" style={{ marginTop: "124px" }}>
                         {getHeartIcon(restaurant.isCustomerFavourite)}
                     </Card.ImgOverlay>
-                    <Card.Body className="pl-0">
-                        <Card.Title>{restaurant.restaurantName}</Card.Title>
-                        <Card.Text style={{ fontSize: "14px" }}>
-                            <span>{getDeliveryFeeSpan(restaurant.freeDelivery, restaurant.deliveryTime)}</span>
-                            <span>{getPriceRange(restaurant.meal)}</span>
-                        </Card.Text>
+                    <Card.Body className="pl-0 pr-0">
+                        <Row>
+                            <Col xs={10}>
+                                <Card.Title style={{ fontSize: "18px" }}>{restaurant.restaurantName}</Card.Title>
+                                <Card.Text style={{ fontSize: "14px" }}>
+                                    <span>{getDeliveryFeeSpan(restaurant.freeDelivery, restaurant.deliveryTime)}</span>
+                                    <span>{getPriceRange(restaurant.meal)}</span>
+                                </Card.Text>
+                            </Col>
+                            <Col className="my-auto">
+                                {getRatingSpan(restaurant.rating)}
+                            </Col>
+                        </Row>
                     </Card.Body>
                     <Card.Footer className="bg-white pl-0">
                         <small className="text-muted">{restaurant.restaurantSpecialty.split(",").join(bullet)}</small>
